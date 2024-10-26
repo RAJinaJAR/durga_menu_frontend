2ea92643878e0d7104e510759aa670de713a238a
RAJinaJAR <116557420+RAJinaJAR@users.noreply.github.com>
2024-10-26 15:53:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/MenuItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MenuItem({ name, desc, price, topping, toppingPrice, image }) {
+  return (
+    <div className="MenuItem">
+      <img
+        src={image || "default-image.jpg"}
+        alt={name}
+        className="MenuItem-image"
+      />
+      <h3>{name}</h3>
+      <p>{desc}</p>
+      {/* <p>Price: ₹{price}</p> */}
+      <br></br>
+      {topping && topping !== "None" && <p>Toppings available: {topping}</p>}
+    </div>
+  );
+}
+
+export default MenuItem;
